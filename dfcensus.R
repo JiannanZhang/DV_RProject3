ccffa189ae91984c8bb1b5ab09177c2a607d44f1
unknown <jaclyncnguyen@utexas.edu>
2015-02-25 10:52:49 -0600

    added census data

---
+++ dfcensus.R
@@ -0,0 +1,5 @@
+require("jsonlite")
+require(dplyr)
+
+q = 'CENSUS2010POP'
+dfpop <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select cityname," \\\""q"\\\" from DIAMONDS"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER=C##cs329e_JCN565, PASS='orcl_JCN565', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', q=q), verbose = TRUE)))
