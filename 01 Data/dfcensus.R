aef54586539f42d846ca8eb172fb38d47dff2970
unknown <jaclyncnguyen@utexas.edu>
2015-03-02 12:31:49 -0600

    updated RMD and Data

--- 01 Data/dfcensus.R
+++ 01 Data/dfcensus.R
@@ -3,5 +3,5 @@ require(dplyr)
 
 q = 'CENSUS2010POP'
 i = 'STNAME'
-k = 'CTYNAME'
+k = 'UPPER(RECIPIENT_COUNTY_NAME)'
 dfpop <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select  "i" ,  "k" ,  "q"  from CENSUSCOUNTY"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_JCN565', PASS='orcl_jcn565', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', q=q, i=i, k=k), verbose = TRUE)))
