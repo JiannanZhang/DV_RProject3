9f9b4fb060a41b53342d69b8c751fb74f7b4fe36
unknown <jaclyncnguyen@utexas.edu>
2015-03-02 23:03:35 -0600

    additional data

--- 01 Data/dfcensus.R
+++ 01 Data/dfcensus.R
@@ -2,6 +2,8 @@ require("jsonlite")
 require(dplyr)
 
 q = 'CENSUS2010POP'
-i = 'STNAME'
+i = 'UPPER(PRINCIPAL_PLACE_STATE)'
 k = 'UPPER(RECIPIENT_COUNTY_NAME)'
 dfpop <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select  "i" ,  "k" ,  "q"  from CENSUSCOUNTY"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_JCN565', PASS='orcl_jcn565', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', q=q, i=i, k=k), verbose = TRUE)))
+
+df_state <- dfpop %>% select (STNAME, CENSUS2010POP) %>% group_by(STNAME) %>% summarise(sum(CENSUS2010POP))
