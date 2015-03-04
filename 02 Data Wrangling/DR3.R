641da8de5848b8f349941f6f7b2d63a9327c2228
unknown <jaclyncnguyen@utexas.edu>
2015-03-03 23:04:17 -0600

    Formatting and adding text

--- 02 Data Wrangling/DR3.R
+++ 02 Data Wrangling/DR3.R
@@ -1,1 +1,1 @@
-df_06_city_total_fund <- df_06_city_spending %>% select(RECIPIENT_CITY_NAME) %>% group_by(RECIPIENT_CITY_NAME) %>% summarize(total_spending = sum(df_06_city_spending$FED_FUNDING_AMOUNT_06))
+df_06_city_total_fund <- df_06_city_spending %>% select(RECIPIENT_CITY_NAME, FED_FUNDING_AMOUNT_06) %>% group_by(RECIPIENT_CITY_NAME) %>% summarise(total_spending = sum(FED_FUNDING_AMOUNT_06))
--- 02 Data Wrangling/DR4.R
+++ 02 Data Wrangling/DR4.R
@@ -1,1 +1,1 @@
-df_07_city_total_fund <- df_07_city_spending %>% select(RECIPIENT_CITY_NAME) %>% group_by(RECIPIENT_CITY_NAME) %>% summarize(total_spending = sum(df_07_city_spending$FED_FUNDING_AMOUNT_07))
+df_07_city_total_fund <- df_07_city_spending %>% select(RECIPIENT_CITY_NAME, FED_FUNDING_AMOUNT_07) %>% group_by(RECIPIENT_CITY_NAME) %>% summarise(total_spending = sum(FED_FUNDING_AMOUNT_07))
